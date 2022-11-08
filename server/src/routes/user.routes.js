d9c875bba1c7ba7be30455ad50342e3811f57c40
Chong Gao & Tomoki Sakamoto <gc605997852@gmail.com>
2022-11-08 21:45:55 +1100

    follow

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,22 +1,24 @@
 module.exports = (express, app) => {
-  const controller = require("../controllers/user.controller.js");
-  const router = express.Router();
+  const controller = require("../controllers/user.controller.js")
+  const router = express.Router()
 
   // Select all users.
-  router.get("/profile", controller.getProfile);
+  router.get("/profile", controller.getProfile)
 
-  router.put("/profile", controller.updateProfile);
+  router.put("/profile", controller.updateProfile)
 
-  router.delete("/profile", controller.deleteProfile);
+  router.delete("/profile", controller.deleteProfile)
 
-  router.get("/search", controller.searchUsers);
+  router.get("/search", controller.searchUsers)
 
-  router.get("/followUsers", controller.getFollowUsers);
-  
-  router.post("/follow/:user_id", controller.followUser);
+  router.get("/userList", controller.getUserList)
 
-  router.post("/unfollow/:user_id", controller.unfollowUser);
-  
+  router.get("/followUsers", controller.getFollowUsers)
+
+  router.post("/follow/:user_id", controller.followUser)
+
+  router.post("/unfollow/:user_id", controller.unfollowUser)
+
   // Add routes to server.
-  app.use("/api/users", router);
-};
+  app.use("/api/users", router)
+}
